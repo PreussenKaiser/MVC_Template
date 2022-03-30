60fb449bf727f9454f03d01e8882eef51afe9aae
Karl Lukan <klukan20@gmail.com>
2022-03-29 23:35:52 -0500

    removed erroneous includes

--- Public/index.php
+++ Public/index.php
@@ -15,11 +15,7 @@ require_once('../Routes/Dispatcher.php');
 require_once('../App/Controllers/Controller.php');
 require_once('../App/Views/View.php');
 require_once('../App/Models/Model.php');
-require_once('../App/Models/Teams.php');
-require_once('../App/Models/Games.php');
-include_once('../App/Models/Logs.php');
 require_once('../Core/Database.php');
 include_once('../Core/Logger.php');
-require_once('../Core/Validation.php');
 
 Dispatcher::dispatch();
