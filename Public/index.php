68f0a1e761d1ce5bdc1a7ff51b4038abeded6c03
Karl Lukan <klukan20@gmail.com>
2022-04-20 14:36:10 -0500

    added autoloader

--- Public/index.php
+++ Public/index.php
@@ -1,23 +1,11 @@
 <?php
-// TODO: Use an autoloader.
 use Config\Config;
 use Routes\Dispatcher;
 
-require('../Routes/Server.php');
-require_once('../Config/Config.php');
+// autoloader
+require_once __DIR__ . '/../vendor/autoload.php';
 
-Config::loadConfig();
 session_start();
 
-require_once('../Routes/Router.php');
-require_once('../Routes/Request.php');
-require_once('../Routes/Dispatcher.php');
-
-require_once('../App/Controllers/Controller.php');
-require_once('../App/Views/View.php');
-require_once('../App/Models/Model.php');
-require_once('../Core/Database.php');
-include_once('../Core/Logger.php');
-require_once('../Core/Validator.php');
-
+Config::loadConfig();
 Dispatcher::dispatch();
